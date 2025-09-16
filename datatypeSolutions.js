208c4586fe6d3136318d2eb7591ec8e7b71ae6a4
jchiu233 <jchiu233@uwo.ca>
2025-09-16 14:38:03 -0400

    Add files via upload

---
+++ datatypeSolutions.js
@@ -0,0 +1,36 @@
+/*
+The goal of this exercise is to practice working with different data types, 
+and understand conversions and basic comparison.
+*/
+
+// remember that strings go in quotes
+let ageString = "18"; // set age to "18"
+
+// lets try again as a number
+let age = 18; // set age to 18
+
+// use a comparison (< or >) to determine if age is at least 18
+// note the variable type
+let isAdult = age >= 18; // true if 18 or older, false otherwise
+
+// change the following line so "something" is undefined
+let something; // stays undefined unless you assign a value
+
+// change the following line so "nothing" is null
+// note the type!
+let nothing = null;
+
+console.log(ageString);        // 18
+console.log(typeof ageString); // string
+
+console.log(age);              // 18
+console.log(typeof age);       // number
+
+console.log(isAdult);          // true (because age = 18)
+console.log(typeof isAdult);   // boolean
+
+console.log(something);        // undefined
+console.log(typeof something); // undefined
+
+console.log(nothing);          // null
+console.log(typeof nothing);   // object
